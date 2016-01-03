ed91aa19440b28e0459b9ecf65480caaa5443b4a
Wendachye <wenda0216@gmail.com>
2016-01-04 01:26:52 +0800

    wenda

--- app/src/main/java/com/example/wenda/tarucnfc/Activity/LoginActivity.java
+++ app/src/main/java/com/example/wenda/tarucnfc/Activity/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.wenda.tarucnfc.Activity;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import com.example.wenda.tarucnfc.R;
 
@@ -10,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //set title to center
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.login_title);
     }
 }
