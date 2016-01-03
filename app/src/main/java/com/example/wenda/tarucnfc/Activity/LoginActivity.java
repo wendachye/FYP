8fc99af7a6137d2de890f8b7d00ed8a23e37770a
Wendachye <wenda0216@gmail.com>
2016-01-03 18:36:45 +0800

    wenda

---
+++ app/src/main/java/com/example/wenda/tarucnfc/Activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.wenda.tarucnfc.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.wenda.tarucnfc.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
