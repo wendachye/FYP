dbd73404dd9c975ab8841672d664840a83ad8ecf
Wendachye <wenda0216@gmail.com>
2016-01-04 16:49:35 +0800

    wenda

--- app/src/main/java/com/example/wenda/tarucnfc/Activity/EditAccountActivity.java
+++ app/src/main/java/com/example/wenda/tarucnfc/Activity/EditAccountActivity.java
@@ -27,7 +27,7 @@ public class EditAccountActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_account, menu);
+        getMenuInflater().inflate(R.menu.menu_save_button, menu);
         return true;
     }
 
