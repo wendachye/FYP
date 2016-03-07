e71b625d6ee337b2f2344e4a575a51aa715ff34d
Wendachye <wenda0216@gmail.com>
2016-03-07 22:00:10 +0800

    done food transaction history

---
+++ app/src/main/java/com/example/wenda/tarucnfc/Fragments/AddNewFoodMenuFragment.java
@@ -0,0 +1,29 @@
+package com.example.wenda.tarucnfc.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wenda.tarucnfc.R;
+
+public class AddNewFoodMenuFragment extends Fragment {
+
+
+    public AddNewFoodMenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add_new_food_menu, container, false);
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/example/wenda/tarucnfc/Fragments/MaintainFoodMenuFragment.java
+++ app/src/main/java/com/example/wenda/tarucnfc/Fragments/MaintainFoodMenuFragment.java
@@ -2,15 +2,25 @@ package com.example.wenda.tarucnfc.Fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.wenda.tarucnfc.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MaintainFoodMenuFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
 
     public MaintainFoodMenuFragment() {
@@ -24,7 +34,49 @@ public class MaintainFoodMenuFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_maintain_food_menu, container, false);
 
+        viewPager = (ViewPager) view.findViewById(R.id.maintain_foodMenu_viewpager);
+        setupViewPager(viewPager);
+        tabLayout = (TabLayout) view.findViewById(R.id.maintain_foodMenu_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
         return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this.getFragmentManager());
+        adapter.addFragment(new AddNewFoodMenuFragment(), "Add Food Menu");
+        adapter.addFragment(new EditFoodMenuFragment(), "Edit Food Menu");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = mFragmentList.get(position);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
 }
